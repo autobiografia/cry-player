d9a97bba24d65b1e9ba220bfabc1c84a870afffb
heyo <otaldokknvofala@gmail.com>
2022-10-09 04:34:26 -0300

    some changes on index

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+
 var cloudscraper = require('cloudscraper');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 9001;
+
+var cors = require('cors');
+
+
+var path = require('path');
 
 function getStr(str, first_character, last_character) {
   if (str.match(first_character + "(.*)" + last_character) == null) {
@@ -9,30 +19,23 @@ function getStr(str, first_character, last_character) {
   }
 }
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 9001;
-
-var cors = require('cors')
-
 app.use(cors());
 
 app.engine('html', require('ejs').renderFile);
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-  res.render(res.render(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/index.html'))
 })
 app.get('/video', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin','*');
-	if(req.query.q){
+	if(!req.query.q){
+		res.end(JSON.stringify({video_config_media:false}));
+	}else {
 		e = cloudscraper.get(req.query.q);
 		e.then(function(e){
 			file = getStr(e, "vilos.config.media = ", ";") || false;
 			seila = JSON.stringify(JSON.parse(file)) || false;
 			res.end(JSON.stringify({video_config_media:seila}));
 		}).catch(function(e){res.end(JSON.stringify({video_config_media:false}))})
-	}else {
-		res.end(JSON.stringify({video_config_media:false}));
 	}
 });
 
